b678b026517f9c586ea9fd80274c51ebfec6f5ac
Ishita <ishitajain385@gmail.com>
2024-03-13 15:36:21 +0530

    committed changes

--- index.js
+++ index.js
@@ -1,101 +1,75 @@
 
-const brush = document.querySelector(".brush");
-const eraser = document.querySelector(".eraser");
-const pencil = document.querySelector(".pencil"); 
-const canvas = document.getElementById("drawingCanvas");
-const ctx = canvas.getContext("2d");
+    const canvas = document.querySelector("canvas");
+ const pencil = document.querySelector(".pencil");
+ const eraser = document.querySelector(".eraser") ;
+ const shapes= document.querySelector(".shapes");
+ const allshapes= document.querySelector(".allshapes");
+ 
+ const clear = document.querySelector(".clear");
+ctx= canvas.getContext("2d");
+
+let isDrawing= false;
+let erasered= false;
+window.addEventListener("load",()=>{
+  canvas.width= canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+
+})
+
+const drawing= (e) =>{
+  if(!isDrawing) return;
+  {
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  if(erasered== true) {
+    ctx.strokeStyle= "white";
+      }
+      else{
+        ctx.strokeStyle= "black";
+      }
+  }
+
+
   
-let isDrawing = false;
-// let lastX, lastY;
-let color = "black";
-let brushSize = 2;
-
-pencil.addEventListener("click", function () {
-  isDrawing = !isDrawing;
-});
-
-
-canvas.addEventListener("mousemove", drawLine);
-canvas.addEventListener("mousedown", startPosition);
-canvas.addEventListener("mouseup", endPosition);
-canvas.addEventListener("mouseout", endPosition);
-
-function startPosition(e) {
-  isDrawing = true;
-  draw(e);
 }
+
+
+     pencil.addEventListener("click" ,()=>{
+      erasered= false;
+      canvas.addEventListener("mousedown", startPosition);
+      canvas.addEventListener("mousemove",drawing);
+      canvas.addEventListener("mouseup", endPosition);    
+       
+     }
+     )
+
+ 
+ 
+function startPosition() {
+  isDrawing = true;
+  
+}
+
 function endPosition() {
   isDrawing = false;
-  ctx.beginPath(); // Reset the path
+  ctx.beginPath(); 
 }
-function drawLine(e) {
-  if (!isDrawing) return;
-
-  draw(e);
-}
-function draw(e) {
-  let x = e.clientX - canvas.offsetLeft;
-  let y = e.clientY - canvas.offsetTop;
-
-  ctx.lineWidth = brushSize;
-  ctx.lineCap = "round";
-  ctx.strokeStyle = color;
-
-  ctx.lineTo(x, y);
-  ctx.stroke();
-  ctx.beginPath();
-  ctx.moveTo(x, y);
+let shapesvisible= false;
+shapes.addEventListener("click", ()=>{
+if(!shapesvisible) {
+allshapes.style.opacity = 1;
+shapesvisible= true;
+} else{
+  allshapes.style.opacity = 0;
+shapesvisible= false;
 }
 
+})
 
+eraser.addEventListener("click",() =>{
+erasered = true;
+})
 
-
-// function drawLine(e) {
-//     if (!isDrawing) return;
-
-   
-//     let x = e.pageX - canvas.offsetLeft;
-//     let y = e.pageY - canvas.offsetTop;
-
-//     ctx.lineWidth = brushSize;
-//     ctx.lineCap = "round";
-//     ctx.strokeStyle = "black";
-
-    
-//     ctx.beginPath();
-//     ctx.moveTo(x, y);
-//     ctx.lineTo(lastX, lastY);
-//     ctx.stroke();
-
-//     lastX = x;
-//     lastY = y;
-//   }
-
-
-//     pencil.addEventListener("click", function () {
-//     isDrawing = !isDrawing; ;
-
-//     if(isDrawing) {
-//       lastX=null;
-//       lastY= null;
-//     }
-//     });
-
-//     canvas.addEventListener("mousemove", drawLine);
-//     canvas.addEventListener("mouseup", function () {
-//       isDrawing = false;
-//     });
-    
-//     pencil.addEventListener("mousemove", function (e) {
-//       // Set the starting position when moving over the pencil icon
-//       if (isDrawing) {
-//         lastX = e.pageX - canvas.offsetLeft;
-//         lastY = e.pageY - canvas.offsetTop;
-//       }
-//     });
-    
-
-
-     
- 
- 
+clear.addEventListener("click", ()=>{
+  ctx.clearRect(0, 0, canvas.width, canvas.height); 
+})
